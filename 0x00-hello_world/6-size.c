807e0b57132d6c73f667f9d18fafe5fe819e2f99
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-08 14:00:55 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char is %lu.\n", (unsigned long)size of (c));
+
+	printf("Size of an int is %lu.\n", (unsigned long)size of (i));
+
+	printf("Size of a long int is %lu.\n", (unsigned long)size of (li));
+
+	printf("Size of a long long int is %lu.\n", (unsigned long)size of (lli));
+
+	printf("Size of a float is %lu.\n", (unsigned long)size of (f));
+
+	return (0);
+}
