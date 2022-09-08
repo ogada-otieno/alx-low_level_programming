7730ea9f469d2bb4bb4754fc78cd5dc4935cae12
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-08 15:46:07 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,15 @@
 #include <unistd.h>
 
 /*
- * main - main block
+ * main - Entry point
  *
- * return: 1
+ * return: Always 1 (Success)
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, str1, 59);
+
 	return (1);
 }
