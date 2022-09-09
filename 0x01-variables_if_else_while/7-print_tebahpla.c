944d2ffaa71e2b298453f4d0a2217f1702e69a5c
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-09 13:42:33 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: printing the alphabet in reverse lowercase
+ * Return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
