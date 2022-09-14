d61b531b0d8c99fa48bfdef9a5d1a9480b745b22
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-14 04:06:44 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = '_putchar\n';
+	putchar(ch);
+	return(0);
+}
