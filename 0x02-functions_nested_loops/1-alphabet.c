f17d7e922c3a4f4f26c86818084cff4bb2dcd644
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-14 05:33:22 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry block
+ * Description: function that prints the alphabet, in lowercase, followed by a new line
+ * Prototype: void print_alphabet(void);
+ * only use _putchar twice in your code
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
