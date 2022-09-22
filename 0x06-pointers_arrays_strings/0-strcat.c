063080914030f71c794834c793fe38061314b498
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-22 07:26:21 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,12 +13,13 @@ char *_strcat(char *dest, char *src)
 
 	for (a = 0; dest[a] != '\0'; a++)
 		;
+
 	for (b = 0; src[b] != '\0'; b++)
 	{
 		dest[a] = src[b];
 		a++;
 	}
+
 	dest[a] = '\0';
-	
 	return (dest);
 }
