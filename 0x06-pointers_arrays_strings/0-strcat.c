16b17710788d5077321eadf23eff17ff774ff2e1
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-22 06:50:42 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - function appends the src string to the dest string
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *dest[20];
+	char *src[];
+	_putchar("%s", *dest);
+
+	return (0);
+}
