f2d8def29fb50d6e14ab45aa862d3554b093318e
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-22 13:08:37 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,35 +3,26 @@
 /**
  * leet - encode a string into 1337
  * @s: string
- * Letters a and A should be replaced by 4
- * Letters e and E should be replaced by 3
- * Letters o and O should be replaced by 0
- * Letters t and T should be replaced by 7
- * Letters l and L should be replaced by 1
- * can only use one if in your code
- * can only use two loops in your code
- * not allowed to use switch
- * not allowed to use any ternary operation
- * Return: encoded string 's'
+ * @n: input
+ * Return: n
  */
 
-char *leet(char *s)
+char *leet(char *n)
 {
-	int i;
-	int j;
+	int i, x; 
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replacer[] = {'4', '3', '0', '7', '1'};
 
-	char alpha[80] = "aAeEoOtTlL";
-	char numbers[80] = "43071";
-
-	for (i = 0; str[i] != '\0'; ++i)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; alpha[j] != '\0'; j++)
+		for (x = 0; x <= 9; x++)
 		{
-			if (str[i] == alpha[j])
+			if (n[i] == find[x])
 			{
-				str[i] = numbers[j / 2];
+				n[i] = replacer[x / 2];
+				x = 9;
 			}
 		}
 	}
-	return (str);
+	return (n);
 }
