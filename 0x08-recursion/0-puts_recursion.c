646b6f2ff56d27405a61cce56ce34d49d3859390
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-28 11:17:18 -0700

    readme and repo

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	_puts_recursion(s[]);
+
+	return (0);
+}
