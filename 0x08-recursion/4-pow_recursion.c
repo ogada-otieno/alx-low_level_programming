3bb24e23737ea27e97b31234cdd8b7ce798f910c
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-28 16:18:07 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,15 +4,12 @@
  * _pow_recursion - function that returns the value of x^y
  * @x: integer base
  * @y: integer exponent
- * @r: integer result
  * If y is lower than 0, the function should return -1
  * Return: x raised to y
  */
 
 int _pow_recursion(int x, int y)
 {
-	int r = _pow_recursion(x, y);
-
 	if (y < 0)
 	{
 		return (-1);
@@ -22,5 +19,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	else
-		return (r);
+		return (x * _pow_recursion(x, y - 1));
 }
