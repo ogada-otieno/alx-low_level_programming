e61fb723327bca3b5cfc283ff95ffc3d87e76436
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-28 14:01:01 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (0);
+	i = 1 + _strlen_recursion(s + 1);
+
+	return (i);
+}
