afdc616ea64962f4e038d2b4709a40cdc991cec7
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-28 17:23:05 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,9 +10,25 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
+	return (helper_function(n, 1));
+}
+
+/**
+ * helper_function - to solve _sqrt_recursion
+ * @c: number to determine if square root
+ * @i: incrementer to compare against `c`
+ * Return: square root if natural square root, or -1 if none found
+ */
+
+int helper_function(int c, int i)
+{
+	int square;
+
+	square = i * i;
+	if (square == c)
+		return (i);
+	else if (square < c)
+		return (halp(c, i + 1));
+	else
 		return (-1);
-	}
-	return (_sqrt_recursion(n));
 }
