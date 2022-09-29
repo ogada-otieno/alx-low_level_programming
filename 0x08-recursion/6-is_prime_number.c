94dced5655095e0eadb17ec5dc1d6055b52ace32
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-28 18:16:34 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,10 +10,22 @@
 
 int is_prime_number(int n)
 {
-	if (n % 2 = 0)
-	{
-		return (1)
-	}
-	else
+	int prime(int i, int no);
+	return (prime((n / 2), n));
+}
+/**
+ * prime - function determines prime numbers
+ * @i: integer argument half the number
+ * @no: integer number being determined
+ * Return: 1 success, 0 failure
+ */
+int prime(int i, int no)
+{
+	if (i < 2)
 		return (0);
+	if (i == 2)
+		return (1);
+	if (no % i == 0)
+		return (0);
+	return (prime(i - 1, no));
 }
