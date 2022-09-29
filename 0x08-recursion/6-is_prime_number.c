57788d16bf638e655e96cbfec80cb0e117e49c99
Rodgers Ogada <ogadarodgers@gmail.com>
2022-09-28 18:07:39 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_prime_number -  function that returns 1
+ * if the input integer is a prime number
+ * otherwise return 0
+ * @n: integer
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 2 = 0)
+	{
+		return (1)
+	}
+	else
+		return (0);
+}
