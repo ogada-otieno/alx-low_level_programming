153c3fac76d818504b192d36f40b19ec8583c4e4
Rodgers Ogada <ogadarodgers@gmail.com>
2022-10-07 13:27:05 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (new_ptr);
 	}
 
-	if (new_size == 0 && ptr)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	new_ptr = malloc(new_size);
 
-	if (!new_ptr)
+	if (new_ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < old_size; i++)
