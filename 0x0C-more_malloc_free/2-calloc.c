384aa7fab65ab6423354adccae825c2d6d0b496c
Rodgers Ogada <ogadarodgers@gmail.com>
2022-10-07 07:27:47 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ar;
+	unsigned int i;
 
 	ar = malloc(sizeof(nmemb) * size);
 
@@ -18,6 +19,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ar == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ar[i] = 0;
 	free(ar);
 
 	return (ar);
