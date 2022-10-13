c3e7140bceab84f56e262ce2bc4a110c8f71e6ac
Rodgers Ogada <ogadarodgers@gmail.com>
2022-10-13 08:32:21 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: character string
+ * @f: function with a pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
