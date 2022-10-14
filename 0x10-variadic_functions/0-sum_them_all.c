4db1a169f3e6c42c01948701a7d79370e0d553f9
Rodgers Ogada <ogadarodgers@gmail.com>
2022-10-14 09:04:15 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Sum all parameters
+ * @n: const unsigned int
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list mynumbers;
+
+	unsigned int i;
+	int sum = 0;
+
+	va_start(mynumbers, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(mynumbers, unsigned int);
+	}
+
+	va_end(mynumbers);
+
+	return (sum);
+}
